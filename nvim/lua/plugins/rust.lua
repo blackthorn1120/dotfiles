10f8f13b3aee9b07acaee91ecd522bc32fe34820
Brennan Bates <brennandb98&gmail.com>
2024-06-08 03:12:17 +0000

    update of config to disable mason rust-analyzer

--- nvim/lua/plugins/rust.lua
+++ nvim/lua/plugins/rust.lua
@@ -2,11 +2,11 @@ return {
     {
         "neovim/nvim-lspconfig",
         opts = {
-            servers = {
-                rust_analyzer = {
-                    mason = false,
-                },
-            },
+            setup = {
+                rust_analyzer = function()
+                    return true
+                end,
+            }
         },
     },
 }
